84f50ac8542065082a48fa1f00eec106e18ca6fb
Pete H <p.a.hebden@ncl.ac.uk>
2019-08-20 20:46:04 +0100

    update after moving local files

--- js/main.js
+++ js/main.js
@@ -6,12 +6,20 @@ window.onload = () => {
                .register('./sw.js');
     }
 
-    const stars_total = 100;
+
+    window.audio_1 = new Audio("tests/1.wav");
+    window.audio_2 = new Audio("tests/2.wav");
+    window.audio_3 = new Audio("tests/3.wav");
+    window.audio_4 = new Audio("tests/4.wav");
+
+    
+
+    const stars_total = 400;
     var i;
     var icon;
     for (i=1; i<=stars_total; i++) {
       var ran = Math.random();
-      if (ran < 0.5) {
+      if (ran < 0.33) {
         icon = "star";
       } else {
         icon = "add";
@@ -42,4 +50,31 @@ $(".title").mouseenter(function(){
 
 $(".title").mouseout(function(){
   $(this).html("brightness_3");
+});
+
+$(".title").click(function(){
+
+  // audio_1.onended = function(){
+  //   audio_2.play();
+  // }
+
+  // audio_2.onended = function(){
+  //   audio_3.play();
+  // }
+
+  // audio_3.onended = function(){
+  //   audio_4.play();
+  // }
+
+  for (let i = 1; i < 4; i++) {
+    console.log(i);
+    console.log(window["audio_"+i]);
+    window["audio_"+i].addEventListener("ended", function(){
+      console.log("-> " + (i+1));
+      //console.log(window["audio_"+(i+1)]);
+      window["audio_"+(i+1)].play();
+    });
+  }
+
+  audio_1.play();
 });
